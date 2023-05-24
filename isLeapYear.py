9142cb9dfcccf9ce4cd7b0ea0a088d1b6c66a0fa
Izhar-Ul-Haq <iizharulhaq9@gmail.com>
2023-05-24 13:02:13 +0500

    Is Leap Year? Calendar Python

---
+++ isLeapYear.py
@@ -0,0 +1,9 @@
+import calendar
+
+# Check if any of the Year is a Leap Year
+print(calendar.isleap(2020))
+print(calendar.isleap(2021))
+print(calendar.isleap(2022))
+print(calendar.isleap(2023))
+print(calendar.isleap(2024))
+print(calendar.isleap(2025))
