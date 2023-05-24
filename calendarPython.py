9cfb9f19db4eecf1735a3ce6494e6fa0223a7440
Izhar-Ul-Haq <iizharulhaq9@gmail.com>
2023-05-24 12:50:33 +0500

    Calendar in Python

---
+++ calendarPython.py
@@ -0,0 +1,11 @@
+import calendar
+# User Input
+
+
+year = int(input("Please Enter the Year!!!"))
+month = int(input("Please Enter the Month!!!"))
+
+# display the Entered Data
+
+may = calendar.month(year, month)
+print(may)
