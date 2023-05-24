d534ad42f55efb1ba68759339040cab371568fec
Izhar-Ul-Haq <iizharulhaq9@gmail.com>
2023-05-24 12:56:57 +0500

    Week Day in Calendar Python

---
+++ weekdayNumber.py
@@ -0,0 +1,14 @@
+import calendar
+
+print(f'Monday Weekday Number: {calendar.MONDAY}')
+print(f'Tuesday Weekday Number: {calendar.TUESDAY}')
+print(f'Wednesday Weekday Number: {calendar.WEDNESDAY}')
+print(f'Thursday Weekday Number: {calendar.THURSDAY}')
+print(f'Friday Weekday Number: {calendar.FRIDAY}')
+print(f'Saturday Weekday Number: {calendar.SATURDAY}')
+print(f'Sunday Weekday Number: {calendar.SUNDAY}')
+
+year = 2023
+jan = 1
+print(calendar.MONDAY)
+print(calendar.month(year, jan))
